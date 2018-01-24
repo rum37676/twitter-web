a77c81d4ddd2384a93c76aa0a75c00e85396be34
Matthias <matthias.rudingsdorfer@gmail.com>
2018-01-24 23:08:24 +0100

    Allow access to public folder

--- routes.js
+++ routes.js
@@ -1,7 +1,14 @@
 const MainView = require('./app/controllers/mainview');
+const Assets = require('./app/controllers/assets');
 
 module.exports = [
 
   { method: 'GET', path: '/', config: MainView.main },
 
+  {
+    method: 'GET',
+    path: '/{param*}',
+    config: { auth: false },
+    handler: Assets.servePublicDirectory,
+  },
 ];
